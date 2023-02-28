c6b734692f10c7f10d5b935a0b8dd8e1e5404d78
Cristianps1988 <cristianps_1@hotmail.com>
2023-02-28 08:41:17 -0500

    hero image and text added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='w-11/12 flex flex-col mx-auto bg-red-300'>
-      <h1 className='text-3xl md:text-6xl text-center md:text-left mb-6 mt-3.5 md:mt-2'>Ahorra e invierte de manera inteligente</h1>
-      <h2 className='text-center text-xs md:text-3xl md:text-left'>Obtén Cashback por tus compras y control total sobre tu dinero</h2>
+    <div className='w-11/12 mx-auto grid grid-cols-1 md:grid-cols-5'>
+        <div className='md:col-span-2'>
+            <h1 className='text-3xl md:text-6xl text-center md:text-left mb-6 mt-3.5 md:mt-2'>Ahorra e invierte de manera inteligente</h1>
+            <h2 className='text-center text-xs md:text-3xl md:text-left'>Obtén Cashback por tus compras y control total sobre tu dinero</h2>
+        </div>
+        <div className='md:col-span-3'>
+            <img src="/public/hero.png" alt="hero" className='w-3/5 md:w-full ml-15 mt-9 md:m-0' />
+        </div>
     </div>
+    
   )
 }
 
